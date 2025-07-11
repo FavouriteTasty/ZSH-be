e554232e8f2aead33773374e8b555e79d500177c
FavouriteTasty <19307110112@fudan.edu.cn>
2025-07-11 17:35:34 +0800

    feat: draft hospitalization

---
+++ src/hospitalization/draft.ts
@@ -0,0 +1,80 @@
+import type { Hospitalization } from "@prisma/client";
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+
+import { prisma } from "../prisma/index.js";
+import { filterUndefinedFields } from "../utils/data.js";
+import { logger } from "../utils/logger.js";
+
+const draftGet = new Hono();
+const draftUpsert = new Hono();
+
+const get = async (id: string) => {
+    return await prisma.hospitalizationDraft.findUnique({
+        where: { userProfileId: id },
+    });
+};
+
+draftGet.get("/:id", async (c) => {
+    try {
+        const id = c.req.param("id");
+        const hospitalization = await get(id);
+        return c.json(hospitalization);
+    } catch (error) {
+        logger(
+            "error",
+            (error as Error).name,
+            (error as Error).message,
+            (error as Error).stack ?? "",
+        );
+        throw new HTTPException(400, {
+            message: "Bad profile id",
+            cause: error,
+        });
+    }
+});
+
+const upsert = async (data: Hospitalization, id: string) => {
+    const filtered = filterUndefinedFields(data);
+
+    await prisma.hospitalizationDraft.upsert({
+        where: {
+            userProfileId: id,
+        },
+        update: filtered,
+        create: filtered,
+    });
+};
+
+const validate = (hospitalization: Hospitalization) => {
+    if (hospitalization === undefined) {
+        throw new HTTPException(400, {
+            message: "Hospitalizations is undefined",
+        });
+    }
+};
+
+draftUpsert.post("/", async (c) => {
+    try {
+        const body = await c.req.json();
+        const profileId = body.id;
+        const data = body.data;
+        validate(data);
+        data.userProfileId = profileId;
+        await upsert(data, profileId);
+        return c.json("create hospitalization success");
+    } catch (error) {
+        logger(
+            "error",
+            (error as Error).name,
+            (error as Error).message,
+            (error as Error).stack ?? "",
+        );
+        throw new HTTPException(400, {
+            message: "Bad hospitalization",
+            cause: error,
+        });
+    }
+});
+
+export { draftGet, draftUpsert };
--- src/hospitalization/index.ts
+++ src/hospitalization/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 
+import { draftGet, draftUpsert } from "./draft.js";
 import get from "./get.js";
 import upsert from "./upsert.js";
 
@@ -7,5 +8,7 @@ const hospitalization = new Hono();
 
 hospitalization.route("upsert", upsert);
 hospitalization.route("get", get);
+hospitalization.route("draftUpsert", draftUpsert);
+hospitalization.route("draftGet", draftGet);
 
 export { hospitalization };
